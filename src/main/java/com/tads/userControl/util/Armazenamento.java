5b58903717adc39b1799b82e6efab5265117dd63
upcode maquina1 <upcode maquina1>
2016-12-09 17:54:06 -0200

    Métodos de busca na classe Armazenamento

--- src/main/java/com/tads/userControl/util/Armazenamento.java
+++ src/main/java/com/tads/userControl/util/Armazenamento.java
@@ -1,24 +1,52 @@
 package com.tads.userControl.util;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.tads.userControl.model.Usuario;
 
 public class Armazenamento {
-	private List<Usuario> usuarios;
+	private static List<Usuario> usuarios = new ArrayList<Usuario>();
 
-	public Armazenamento(){
-		usuarios = new ArrayList<>();
+	public void adicionarUsuario(Usuario usuario){
+		Armazenamento.usuarios.add(usuario);
 	}
 	
+	public List<Usuario> encontrarUsuariosPorNome(String nomeFiltro){
+		return Armazenamento.usuarios.stream().filter(u -> u.getNome().contains(nomeFiltro)).collect(Collectors.toList());
+	}
+	
+	public List<Usuario> encontrarUsuariosPorCpf(String cpfFiltro){
+		return Armazenamento.usuarios.stream().filter(u -> u.getNome().equals(cpfFiltro)).collect(Collectors.toList());
+	}
+	
+	public List<Usuario> encontrarUsuariosPorEmail(String emailFiltro){
+		return Armazenamento.usuarios.stream().filter(u -> u.getNome().equals(emailFiltro)).collect(Collectors.toList());
+	}
+	
+	public List<Usuario> encontrarUsuariosPorData(Date data){
+		LocalDate dataFiltro = data.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		return Armazenamento.usuarios.stream().filter(u -> u.getDataNascimento().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().isEqual(dataFiltro)).collect(Collectors.toList());
+	}
+	
+	public List<Usuario> encontrarUsuariosMaioresDeIdade(Date data){
+		LocalDate dataFiltro = data.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		LocalDate hoje = LocalDate.now();
+		return Armazenamento.usuarios.stream().filter(u -> u.getDataNascimento().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().isEqual(dataFiltro)).collect(Collectors.toList());
+	}
+	
+	public List<Usuario> encontrarUsuariosMenoresDeIdade(Date data){
+		return null;
+	}
 	
 	public List<Usuario> getUsuarios() {
-		return usuarios;
-	}
-
-	public void setUsuarios(List<Usuario> usuarios) {
-		this.usuarios = usuarios;
+		return Armazenamento.usuarios;
 	}
 	
 }
